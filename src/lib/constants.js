c794fc27f40239b5744d796bb31175fc5ffb0a8d
deCipher <decipher@kali.kali>
2026-07-20 11:52:19 +0300

    Implemented AuthContext

---
+++ src/lib/constants.js
@@ -0,0 +1,21 @@
+export const CURRENCIES = ["USD", "EUR", "GBP", "KES", "AUD"];
+
+export const FREQUENCIES = ["daily", "weekly", "monthly", "yearly"];
+
+export const CATEGORIES = [
+  "entertainment",
+  "utilities",
+  "health",
+  "education",
+  "other",
+];
+
+export const PAYMENT_METHODS = [
+  "credit_card",
+  "debit_card",
+  "paypal",
+  "bank_transfer",
+  "mobile_money",
+];
+
+export const STATUSES = ["active", "expired", "canceled"];
